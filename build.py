7b69ee6de0d18259a351403c1c63057ac1a6c9e8
wangye <wangye@microsoft.com>
2021-07-27 00:49:36 +0000

    update

--- build.py
+++ build.py
@@ -1,8 +1,12 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import os
 from pathlib import Path
 import requests
 import shutil
 import sys
+import subprocess
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -17,7 +21,7 @@ def resolve_executable_path(command_or_path):
     else:
         return None
 
-clean = True
+clean = False
 
 if __name__ == "__main__":
 
@@ -28,19 +32,19 @@ if __name__ == "__main__":
         sys.exit(0)
 
     # different os?
-    Path(SCRIPT_DIR + "/package").mkdir(parents=True, exist_ok=True)
     Path(SCRIPT_DIR + "/build").mkdir(parents=True, exist_ok=True)
+    Path(SCRIPT_DIR + "/build" + "/ort_package").mkdir(parents=True, exist_ok=True)
 
     url = 'https://www.nuget.org/api/v2/package/Microsoft.ML.OnnxRuntime.Gpu/1.8.1'
+    print("Downloading Onnxruntime package from", url, "...")
     r = requests.get(url, allow_redirects=True)
 
-    pkg_path = Path(SCRIPT_DIR + '/package' + '/ort.zip')
+    pkg_path = Path(SCRIPT_DIR + "/build" + '/ort_package' + '/ort.zip')
     open(pkg_path, 'wb').write(r.content)
-
+    print("Unpacking Onnxruntime package ...")
     shutil.unpack_archive(pkg_path, pkg_path.parent.absolute())
 
-    #cmake_path = resolve_executable_path(args.cmake_path)
-    #ctest_path = None if args.use_vstest else resolve_executable_path(args.ctest_path)
-    #build_dir = args.build_dir
-    #script_dir = os.path.realpath(os.path.dirname(__file__))
-    #source_dir = os.path.normpath(os.path.join(script_dir, "..", ".."))
+    cmake_path = resolve_executable_path("cmake")
+
+    subprocess.run([cmake_path, ".."], cwd = SCRIPT_DIR + "/build")
+    subprocess.run([cmake_path, "--build", "."], cwd = SCRIPT_DIR + "/build")
